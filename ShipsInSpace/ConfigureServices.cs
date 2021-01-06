f28abe9bd1e1ec4bc3a15679529dd8b6d52a4dd3
Job <LordChunk@users.noreply.github.com>
2021-01-06 14:53:28 +0100

    Reverted migration change and added role policy

---
+++ ShipsInSpace/ConfigureServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ShipsInSpace
+{
+    public static class ConfigureServices
+    {
+        public static void AddPolicies(this IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Manager", policy => policy.RequireRole("Manager"));
+            });
+        }
+    }
+}
--- ShipsInSpace/Startup.cs
+++ ShipsInSpace/Startup.cs
@@ -28,6 +28,8 @@ namespace ShipsInSpace
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
