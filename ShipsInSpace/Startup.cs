757d70d559fd22316c3f4be3f53d4330c801aa18
Job <LordChunk@users.noreply.github.com>
2021-01-06 15:19:49 +0100

    Fixed policies

--- ShipsInSpace/Startup.cs
+++ ShipsInSpace/Startup.cs
@@ -32,6 +32,8 @@ namespace ShipsInSpace
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
+            services.AddPolicies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
