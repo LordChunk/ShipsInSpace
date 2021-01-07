f71f953a4529ca6c665137da58c6cfe48058949f
Mark Vermulst <m.vermulst@student.avans.nl>
2021-01-07 13:13:29 +0100

    Setup for step 3 and 4

---
+++ ShipsInSpace/Models/SelectionOfShipModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GalacticSpaceTransitAuthority;
+
+namespace ShipsInSpace.Models
+{
+    public class SelectionOfShipModel
+    {
+        public Ship SelectedShip{ get; set; }
+        public List<Hull> AllHulls{ get; set; }
+        public List<Engine> AllEngines { get; set; }
+        public List<Wing> AllWings { get; set; }
+        public List<Weapon> AllWeapons { get; set; }
+    }
+}
