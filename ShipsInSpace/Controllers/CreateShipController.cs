2fec21632d0791c70228fddec6ada60d16e8ab91
Mark Vermulst <m.vermulst@student.avans.nl>
2021-01-07 13:27:41 +0100

    renamed methods and setup for the body of the CreateShipController

--- ShipsInSpace/Controllers/CreateShipController.cs
+++ ShipsInSpace/Controllers/CreateShipController.cs
@@ -1,20 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShipsInSpace.Models;
 
 namespace ShipsInSpace.Controllers
 {
     public class CreateShipController : Controller
     {
-        public IActionResult StepOne()
+        public IActionResult Step1(SelectionOfShipModel model = null)
+        {
+            if (model == null)
+            {
+                model = new SelectionOfShipModel();
+            }
+            return View("CreateShip", model);
+        }
+
+        [HttpPost]
+        public IActionResult Step1Confirm(SelectionOfShipModel model)
+        {
+            if (!ValidateChoicesStep1(model))
+            {
+                RedirectToAction("Step1", model);
+            }
+            return RedirectToAction("Step3", model);
+        }
+        private bool ValidateChoicesStep1(SelectionOfShipModel model)
+        {
+            return true;
+        }
+
+        public IActionResult Step3(SelectionOfShipModel model)
         {
             return View("CreateShip");
         }
 
-        public IActionResult StepTwo()
+        [HttpPost]
+        public IActionResult Step2Confirm(SelectionOfShipModel model)
         {
-            return View("CreateShip");
+            if (!ValidateChoicesStep2(model))
+            {
+                RedirectToAction("Step3", model);
+            }
+            return RedirectToAction("Step3", model);
+        }
+        private bool ValidateChoicesStep2(SelectionOfShipModel model)
+        {
+            return true;
         }
 
-        public IActionResult Confirmation()
+        public IActionResult Step3()
         {
             return View("CreateShip");
         }
