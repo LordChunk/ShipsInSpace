e65048e6146951e61094065ada5247d50b6b8537
Mark Vermulst <m.vermulst@student.avans.nl>
2021-01-08 15:00:47 +0100

    Added logic for the first validation

--- ShipsInSpace/Models/CustomAttributes/TotalWeightNotExceeded.cs
+++ ShipsInSpace/Models/CustomAttributes/TotalWeightNotExceeded.cs
@@ -1,12 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace ShipsInSpace.Models.CustomAttributes
 {
     public class TotalWeightNotExceeded : ValidationAttribute
     {
-        public override bool IsValid(object? value)
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            return base.IsValid(value);
+            if (value == null)
+                return new ValidationResult("Ship needs to be created.");
+
+            var ship = (ShipViewModel)value;
+            
+            if (ship.Hull == null)
+                return new ValidationResult("Hull needs to be selected.");
+           
+            var maxCapacity = (int)ship.Hull.DefaultMaximumTakeOffMass;
+            int currentCapacity = 0;
+
+            if (ship.Engine != null)
+            {
+                currentCapacity += ship.Engine.Weight;
+            }
+
+            if (ship.Wings != null)
+            {
+                currentCapacity += ship.Wings.Sum(wing => wing.Weight);
+            }
+
+            return currentCapacity > maxCapacity ? ValidationResult.Success : new ValidationResult("Current configuration weights too much!");
         }
     }
 }
