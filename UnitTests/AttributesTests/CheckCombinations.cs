926c53ac417673b018409fa3fe72df584f612d13
Job <LordChunk@users.noreply.github.com>
2021-01-10 18:46:57 +0100

    Added unit test project

---
+++ UnitTests/AttributesTests/CheckCombinations.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using ShipsInSpace.Models;
+
+namespace UnitTests.AttributesTests
+{
+    public class CheckCombinations
+    {
+        public ShipViewModel Ship;
+
+        [SetUp]
+        public void Setup()
+        {
+            Ship = new ShipViewModel
+            {
+                
+            }
+        }
+    }
+}
